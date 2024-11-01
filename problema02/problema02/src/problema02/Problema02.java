0c04d04a3f6b7abee8d56f7644b3714e22c977a8
jhordycamacas <jhordycamacaas9@gmail.com>
2024-11-01 09:43:00 -0500

    Cambios 2

--- problema02/problema02/src/problema02/Problema02.java
+++ problema02/problema02/src/problema02/Problema02.java
@@ -4,17 +4,53 @@
  */
 package problema02;
 
-/**
- *
- * @author usuario
- */
+import java.util.Scanner;
+
 public class Problema02 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        
+        String primerhijo;
+        String segundohijo;
+        String tercerhijo;
+        double gastos1;
+        double gastos2;
+        double gastos3;
+        double totalgastos;
+        
+        System.out.println("Ingrese el nombre del primer hijo");
+        primerhijo = entrada.nextLine();
+        System.out.println("Ingrese los gastos del primer hijo");
+        gastos1 = entrada.nextDouble();
+        System.out.println("Ingrese el nombre del segundo hijo");
+        entrada.nextLine();
+        segundohijo = entrada.nextLine();
+        System.out.println("Ingrese gastos del segundo hijo");
+        gastos2 = entrada.nextDouble();
+        System.out.println("Ingrese nombre del tercer hijo");
+        entrada.nextLine();
+        tercerhijo = entrada.nextLine();
+        System.out.println("Ingrese gastos del tercer hijo");
+        gastos3 = entrada.nextDouble();
+        
+        totalgastos = gastos1 + gastos2 + gastos3;
+        
+        System.out.printf("Nombre del primer hijo : %s\n"
+        + "Gastos primer hijo : %.2f\nNombre del segundo hijo : %s\n"
+        + "Gastos segundo hijo : %.2f\nNombre del tercer hijo : %s\n"
+        + "Gastos tercer hijo : %.2f\nEl total de gastos es : %.2f",
+                primerhijo,
+                gastos1,
+                segundohijo,
+                gastos2,
+                tercerhijo,
+                gastos3,
+                totalgastos);
+        
     }
     
 }
