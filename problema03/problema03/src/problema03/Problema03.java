55f2ea62b82e177b9c821e5d0dba8be7698f45d6
jhordycamacas <jhordycamacaas9@gmail.com>
2024-11-01 10:19:34 -0500

    Cambios 3

---
+++ problema03/problema03/src/problema03/Problema03.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema03;
+
+import java.util.Scanner;
+
+public class Problema03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        String nombrescompl;
+        String direccion;
+        double minutos;
+        double costomin;
+        double total_cancelar;
+        
+        System.out.println("Ingrese nombres completos del dueño de la linea");
+        nombrescompl = entrada.nextLine();
+        System.out.println("Ingrese la direccion ");
+        direccion = entrada.nextLine();
+        System.out.println("Ingrese costo por minutos");
+        costomin = entrada.nextDouble();
+        System.out.println("Ingrese numero de minutos consumidos al mes");
+        minutos = entrada.nextDouble();
+        
+        total_cancelar = costomin * minutos;
+        
+        System.out.printf("Reporte : \nNombres completos : %s\n"
+                + "Direccion : %s\nCosto por minutos: %.2f\n"
+                + "Numero de minutos consumidos : %.2f\n"
+                + "Valor a cancelar : %.2f",
+                nombrescompl,
+                direccion,
+                costomin,
+                minutos,
+                total_cancelar);    
+    }
+    
+}
